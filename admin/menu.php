a6c291bf65fd00da6f9e866c67e79f0b157cab97
OWNER <akshay.d.mohan@gmail.com>
2015-02-24 23:04:12 +0530

    created admin and company menu.php

---
+++ admin/menu.php
@@ -0,0 +1,20 @@
+<?php
+/*
+* menu of admin
+*/
+
+	echo '
+		<ul>
+			<li>
+				<a href="home.php?content=forms/companySave.php">
+					Add Company
+				</a>
+			</li>
+			<li>
+				<a href="home.php?content=forms/studentSave.php">
+					Add Student
+				</a>
+			</li>
+		</ul>	
+	';
+?>
---
+++ company/menu.php
@@ -0,0 +1,25 @@
+<?php
+/*
+* menu of company
+*/
+
+	echo '
+		<ul>
+			<li>
+				<a href="home.php?content=company/regStudentList.php">
+					Students List
+				</a>
+			</li>
+			<li>
+				<a href="home.php?content=forms/companyPost.php">
+					Post News
+				</a>
+			</li>
+			<li>
+				<a href="home.php?content=company/oldPosts.php">
+					Old Posts
+				</a>
+			</li>
+		</ul>	
+	';
+?>
